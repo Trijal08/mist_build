baf625f614ebe719bdc35b1f525bbe197c5a5798
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-11-03 18:10:00 +0000

    Add roomservice Partial squash of:
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date: Sun, 1 Jan 2017 21:45:07 +0100
    
        lunch: Use cd - instead of popd
    
        * ZSH breaks a lot of things.
    
        Change-Id: Idee8485bee673bebf6fe5857253306b20aa92eb5
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date: Thu, 5 Jan 2017 16:30:15 +0100
    
        build: Replace pushd with cd
    
        * For consistency
    
        Change-Id: I96711e3be7b73ebadd589f460e5c5e90d000a4f7
    
      Author: Koushik Dutta <koushd@gmail.com>
      Date: Sat, 26 Nov 2011 18:51:42 -0800
    
        roomservice delivers you lunch combos from the CyanogenMod github.
    
        fix roomservice formatting
        support product names with _ in them
        fix roomservice to handle pagination
    
        Change-Id: I4923c2f768094dbad8e06a72d9f27d46414030ab
    
      Author: LuK1337 <priv.luk@gmail.com>
      Date:   Tue Apr 13 19:57:24 2021 +0200
    
        envsetup: Fix the way we check check_product's return value
    
        Change-Id: I5a2a4b7be5dfee198094902aaa2c9a8c27b97704

--- envsetup.sh
+++ envsetup.sh
@@ -116,7 +116,7 @@ EOF
     local T=$(gettop)
     local A=""
     local i
-    for i in `cat $T/build/envsetup.sh $T/vendor/cherish/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
+    for i in `cat $T/build/envsetup.sh $T/vendor/mist/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       A="$A $i"
     done
     echo $A
@@ -127,8 +127,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/cherish/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
-    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/cherish/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/mist/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/mist/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\builtin cd $T; build/soong/soong_ui.bash --dumpvars-mode \
                         --vars="${cached_vars[*]}" \
@@ -832,7 +832,20 @@ function lunch()
         return 1
     fi
 
-    check_product $product
+    if ! check_product $product
+    then
+        # if we can't find a product, try to grab it off the mist X GitHub
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/mist/build/tools/roomservice.py $product
+        cd - > /dev/null
+        check_product $product
+    else
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/mist/build/tools/roomservice.py $product true
+        cd - > /dev/null
+    fi
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
@@ -2106,4 +2119,4 @@ function repopick() {
 }
 export ANDROID_BUILD_TOP=$(gettop)
 
-. $ANDROID_BUILD_TOP/vendor/cherish/build/envsetup.sh
+. $ANDROID_BUILD_TOP/vendor/mist/build/envsetup.sh
