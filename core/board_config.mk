Project-Awaken/android_build

build: Add check for permissive selinux in official builds
Change-Id: Icb2d6df256167cdeaf374c4be6ffb58a9b6c58d2

core/board_config.mk
@@ -309,6 +309,12 @@ ifneq (,$(BOARD_BOOTCONFIG))
   INTERNAL_BOOTCONFIG := $(BOARD_BOOTCONFIG)
 endif
 
+ifneq ($(filter official,$(AWAKEN_BUILD_TYPE)),)
+ifneq ($(filter androidboot.selinux=permissive,$(INTERNAL_KERNEL_CMDLINE)),)
+$(error Permissive selinux in official builds is not allowed)
+endif
+endif
+
 ifneq ($(filter %64,$(TARGET_ARCH)),)
   TARGET_IS_64_BIT := true
 endif

Trijal08/mist_build

core/board_config.mk
@@ -311,6 +311,12 @@ ifneq (,$(BOARD_BOOTCONFIG))
   INTERNAL_BOOTCONFIG := $(BOARD_BOOTCONFIG)
 endif
 
+ifneq ($(filter official,$(EVEREST_BUILD_TYPE)),)
+ifneq ($(filter androidboot.selinux=permissive,$(INTERNAL_KERNEL_CMDLINE)),)
+$(error Permissive selinux in official builds is not allowed)
+endif
+endif
+
 ifneq ($(filter %64,$(TARGET_ARCH)),)
   TARGET_IS_64_BIT := true
 endif